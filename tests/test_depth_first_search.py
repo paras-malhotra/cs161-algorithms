32f57e067e6d112300faac91f00f06209a488ed7
Paras Malhotra <paras@insenseanalytics.com>
2023-11-29 16:04:01 +0530

    Finding bridges

--- tests/test_depth_first_search.py
+++ tests/test_depth_first_search.py
@@ -1,7 +1,7 @@
 import pytest
 from typing import List
 from graph import Graph
-from depth_first_search import depth_first_search, depth_first_search_entire_graph, topological_sort
+from depth_first_search import depth_first_search, depth_first_search_entire_graph, topological_sort, get_bridge_edges
 
 def test_depth_first_search():
     graph = Graph(['u', 'v', 'w', 'x', 'y', 'z'], [('u', 'v'), ('u', 'x'), ('v', 'y'), ('w', 'y'), ('w', 'z'), ('x', 'v'), ('y', 'x')])
@@ -41,3 +41,26 @@ def test_topological_sort(vertices, edges):
     graph = Graph(vertices, edges)
     order = topological_sort(graph)
     assert is_valid_topological_order(graph, order)
+
+@pytest.mark.parametrize("vertices, edges, expected_bridges", [
+    # Acyclic graph with two bridges
+    (['A', 'B', 'C'], [('A', 'B'), ('B', 'C')], [('B', 'C'), ('A', 'B')]),
+
+    # Case with no bridges
+    (['A', 'B', 'C'], [('A', 'B'), ('B', 'C'), ('C', 'A')], []),
+
+    # Cyclic graph with one bridges
+    (['A', 'B', 'C', 'D'], [('A', 'B'), ('B', 'C'), ('C', 'D'), ('D', 'B')], [('A', 'B')]),
+
+    # Complex cyclic graph with one bridge
+    (['A', 'B', 'C', 'D', 'E'], [('A', 'B'), ('A', 'D'), ('B', 'C'), ('C', 'D'), ('D', 'E')], [('D', 'E')]),
+])
+def test_get_bridge_edges(vertices, edges, expected_bridges):
+    graph = Graph(vertices, edges, undirected=True)
+    bridges = get_bridge_edges(graph)
+
+    # Sort bridges for comparison (since order doesn't matter in undirected graph)
+    bridges_sorted = sorted(bridges, key=lambda x: (min(x), max(x)))
+    expected_sorted = sorted(expected_bridges, key=lambda x: (min(x), max(x)))
+
+    assert bridges_sorted == expected_sorted
